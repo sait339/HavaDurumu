f6837ab5dc3e4363a10454186537c15765aa184e
Sait Kocabaş <75066698+sait339@users.noreply.github.com>
2022-05-12 02:16:40 +0300

    Add files via upload

---
+++ Api/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Api
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(37, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hava Durumu Öğren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ankara",
+            "İstanbul",
+            "Konya",
+            "Kahramanmaraş",
+            "Adana",
+            "Antalya",
+            "Bolu",
+            "Bursa",
+            "İzmir",
+            "Uşak"});
+            this.comboBox1.Location = new System.Drawing.Point(37, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hava Durumu İçin Şehir Seçin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(33, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 22);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(33, 373);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 22);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(350, 427);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Api/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace Api
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://weatherapi-com.p.rapidapi.com/current.json?q="+sehir);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "weatherapi-com.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "2fe732f7f7msheb14c799a0a6ac5p14554cjsnf6d25b5654ab");
+            IRestResponse response = client.Execute(request);
+            dynamic js = JsonConvert.DeserializeObject(response.Content);
+            string durum = js.current.condition["text"];
+            string derece = js.current["temp_c"];
+            string nem = js.current["humidity"];
+            string ruzgaryon = js.current["wind_dir"];
+            string ruzgarhiz = js.current["wind_kph"];
+            label2.Text = sehir;
+            label5.Text = "NEM : %" + nem;
+            if (ruzgaryon == "N") { label6.Text = "RÜZGAR YÖNÜ : Kuzey"; }
+            else if (ruzgaryon == "E") { label6.Text = "RÜZGAR YÖNÜ : Doğu"; }
+            else if (ruzgaryon == "S") { label6.Text = "RÜZGAR YÖNÜ : Güney"; }
+            else if (ruzgaryon == "W") { label6.Text = "RÜZGAR YÖNÜ : Batı"; }
+            else if (ruzgaryon == "NE") { label6.Text = "RÜZGAR YÖNÜ : Kuzeydoğu"; }
+            else if (ruzgaryon == "SE") { label6.Text = "RÜZGAR YÖNÜ : Güneydoğu"; }
+            else if (ruzgaryon == "SW") { label6.Text = "RÜZGAR YÖNÜ : Güneybatı"; }
+            else if (ruzgaryon == "NW") { label6.Text = "RÜZGAR YÖNÜ : Kuzeybatı"; }
+            else if (ruzgaryon == "NNE") { label6.Text = "RÜZGAR : Yıldız-Poyraz"; }
+            else if (ruzgaryon == "ENE") { label6.Text = "RÜZGAR : Gündoğusu-Poyraz"; }
+            else if (ruzgaryon == "ESE") { label6.Text = "RÜZGAR : Gündoğusu-Keşişleme"; }
+            else if (ruzgaryon == "SSE") { label6.Text = "RÜZGAR : Kıble-Keşişleme"; }
+            else if (ruzgaryon == "SSW") { label6.Text = "RÜZGAR : Kıble-Lodos"; }
+            else if (ruzgaryon == "WSW") { label6.Text = "RÜZGAR : Batı-Lodos"; }
+            else if (ruzgaryon == "WNW") { label6.Text = "RÜZGAR : Batı-Karayel"; }
+            else if (ruzgaryon == "NNW") { label6.Text = "RÜZGAR : Yıldız-Karayel"; }
+            else { }
+            label7.Text = "RÜZGAR HIZI : " + ruzgarhiz+ " km/s";
+            if (durum == "Sunny") { label3.Text = "Güneşli " + derece + "°"; pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\havadrm\\sunny.png"); }
+            else if (durum == "Rain") { label3.Text = "Yağmurlu " + derece + "°"; pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\havadrm\\rain_light.png"); }
+            else if (durum == "Partly cloudy") { label3.Text = "Parçalı Bulutlu " + derece + "°"; pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\havadrm\\sunny_s_cloudy.png"); }
+            else if (durum == "Cloudy") { label3.Text = "Bulutlu " + derece + "°"; pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\havadrm\\partly_cloudy.png"); }
+            else if (durum == "Mist" || durum == "Fog") { label3.Text = "Sisli " + derece + "°"; pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\havadrm\\mist.png"); }
+            else if (durum == "Overcast") { label3.Text = "Çok Bulutlu " + derece + "°"; pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\havadrm\\cloudy.png"); }
+            else { label3.Text = durum; }
+
+
+        }
+        string sehir;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sehir = comboBox1.Text;
+            button1.Text = sehir + " Hava Durumu Öğren";
+        }
+    }
+}
